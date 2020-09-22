fe69836a1634535bdfdd0f1500e63cc576e5ef82
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-22 22:41:34 +0530

    use case 1

--- src/com/capgemini/SnakeLadder.java
+++ src/com/capgemini/SnakeLadder.java
@@ -4,7 +4,11 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	 int PLAYER_INITIAL_POSITION = 0;
+	 int playerCount = 1;
      System.out.println("Starting Snake and Ladder Problem.");
+     System.out.println("No of players " + playerCount);
+     System.out.println("Initial position of player is " + PLAYER_INITIAL_POSITION);
 	}
 
 }
