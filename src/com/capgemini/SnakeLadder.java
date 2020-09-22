09eae6e05239bc02c6b89d487015e7857d5502d7
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-22 22:35:52 +0530

    print command in main

--- src/com/capgemini/SnakeLadder.java
+++ src/com/capgemini/SnakeLadder.java
@@ -4,7 +4,7 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     System.out.println("Starting Snake and Ladder Problem.");
 	}
 
 }
